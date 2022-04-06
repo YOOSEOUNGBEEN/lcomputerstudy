7cc11590aa070a0f899aade84d00bd27b839ccdb
HyeonJin <jhj960113@gmail.com>
2022-04-06 15:46:51 +0900

    text추가

--- src/com/lcomputerstudy/testmvc/controller/Controller.java
+++ src/com/lcomputerstudy/testmvc/controller/Controller.java
@@ -23,7 +23,7 @@ public class Controller extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html; charset=utf-8");
+		response.setContentType("text/html; charset=utf-8"); // test
 
 		String requestURI = request.getRequestURI();
 		String contextPath = request.getContextPath();
